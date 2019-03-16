c193bd26855b47cdff3d3983797e3d13226843ed
Ara-yjx <jiaxi@ucsb.edu>
2019-03-16 16:45:31 -0700

    this is the real SECURE agent

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-var MQTT = require("async-mqtt");
-
-const topic = 'te/st'
-var client = MQTT.connect("tcp://yejiaxi.cn:1883");
-
-
-client.on("connect", async () => {
-
-	console.log("onConnect:");
-	try {
-		await client.subscribe(topic);
-
-		await client.publish(topic, "It works!");
-		// await client.end();
-    console.log("Done");
-    
-	} catch (e){
-		console.log('ERROR')
-		console.log(e.stack);
-		// process.exit();
-	}
-})
-
-client.on("message", async (topic, message) => {
-  console.log('onMessage:')
-	console.log(message.toString())
-	try {
-		await client.publish(topic, message);
-		// await client.end();
-    console.log("Done");
-    
-	} catch (e){
-		console.log(e.stack);
-		process.exit();
-	}
-})
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+ 
+const server = new https.createServer({
+  cert: fs.readFileSync('./ssl/certificate.crt'),
+  key: fs.readFileSync('./ssl/private.key')
+});
+const wss = new WebSocket.Server({ server });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
+ 
+server.listen(8080);
