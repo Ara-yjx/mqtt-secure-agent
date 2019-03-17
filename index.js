47ff7dd5b2b89b7510e4cb17e50b7399145a0a54
Petry <jiaxi@umail.ucsb.edu>
2019-03-17 08:57:05 +0800

    code done. test pass

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const fs = require('fs');
 const https = require('https');
 const WebSocket = require('ws');
- 
+const MQTT = require("async-mqtt");
+
+const mqtt = MQTT.connect("tcp://broker.mqttdashboard.com:1883");
+
+// var msgQueue = []
+var wsInstance = null
+
+
+mqtt.on("connect", async()=>{
+  try {
+    mqtt.subscribe(process.env.TOPIC || '6107445', function (err) {
+      if (!err) {
+        console.log('Successfully connected and subscribed.')
+      }
+    })
+	} catch (e){
+		console.log(e.stack);
+		process.exit();
+	}
+});
+
+
+function linkMqttTo(wsInstance) {
+  mqtt.on('message', function (topic, message) {
+    console.log('[MQTT]', message.toString())
+    // msgQueue.push(message.toString())
+
+    // if(!wsInstance) {
+      wsInstance.send(message.toString())
+    // }
+  })
+}
+
+
+
 const server = new https.createServer({
-  cert: fs.readFileSync('./ssl/certificate.crt'),
-  key: fs.readFileSync('./ssl/private.key')
+  cert: fs.readFileSync(process.env.sslCrt || './ssl/certificate.crt'),
+  key: fs.readFileSync(process.env.sslKey || './ssl/private.key')
 });
 const wss = new WebSocket.Server({ server });
- 
+
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
- 
-  ws.send('something');
+  
+  ws.send('Server said hello.');
+  linkMqttTo(ws)  
 });
- 
-server.listen(8080);
+
+server.listen(process.env.PORT || 3355);
